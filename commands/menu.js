a8981bcbdc261806a3f80449aeea8117f97052f0
26j01acs009-png <26j01acs009@anu.ac.ke>
2026-09-03 13:15:58 +0300

    c

--- commands/menu.js
+++ commands/menu.js
@@ -18,10 +18,18 @@ module.exports = {
       menuText += `🎨 *𝖬𝖤𝖣𝖨𝖠 & 𝖥𝖴𝖭*\n`;
       menuText += `│ ☛ _.st_ - Convert image to sticker\n`;
       menuText += `│ ☛ _.vv_ - Re-send view-once media\n\n`;
+      menuText += `🎵 *𝖠𝖴𝖣𝖨𝖮 & 𝖲𝖤𝖠𝖱𝖢𝖧*\n`;
+      menuText += `│ ☛ _.tts_ / _.say_ - Convert text to a voice note\n`;
+      menuText += `│ ☛ _.download_ / _.dl_ - Download a social video\n`;
+      menuText += `│ ☛ _.lyrics_ / _.lyric_ - Search for song lyrics\n\n`;
+      menuText += `🎮 *𝖦𝖠𝖬𝖤𝖲*\n`;
+      menuText += `│ ☛ _.ttt_ - Play Tic-Tac-Toe\n\n`;
       menuText += `⚙️ *𝖴𝖳𝖨𝖫𝖨𝖳𝖸*\n`;
       menuText += `│ ☛ _.ginfo_ - Show group information\n`;
       menuText += `│ ☛ _.tr_ - Translate a replied message\n`;
-      menuText += `│ ☛ _.schedule_ - Schedule a message\n\n`;
+      menuText += `│ ☛ _.schedule_ - Schedule a message\n`;
+      menuText += `│ ☛ _.mevv_ - Manage view-once settings\n`;
+      menuText += `│ ☛ _.moderator_ - Manage moderation settings\n\n`;
       menuText += `✨ _Tip: Reply to messages directly to apply commands!_`;
 
       await sock.sendMessage(jid, { text: menuText }, { quoted: msg });
